ad043c34e6004897f6ed94f75f2c0703b989ecca
Richard Ohr <32947983+rohr265@users.noreply.github.com>
2021-08-22 19:44:40 -0400

    how to access grid cells

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var counter = 0;
 var table = document.getElementById("table");
-
+var rows = table.children;
 function makeSomeWords()
 {
-	counter++;
+	
 	var x = document.getElementById("wordsSpace");
 	x.innerHTML = Math.random();
 	var bluePower = 10;
 	var redPower = 20;
 	
-	
-	for (let i = 0; i < table.children.length; i++) {
-		for (let j = 0; j < i.children.length; j++) {
-			j.innerHTML = "ayy";
-		} 
+	for (let j = 0; j < rows[counter].children.length; j++) {
+		rows[counter].children[j].style.backgroundColor = "red";
 	} 
+	
+	counter++;
 	
 }
